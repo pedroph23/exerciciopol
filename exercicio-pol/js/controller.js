3eec11776f1e359f42452eee6cadc1332ad1fae7
pedroph23 <30670185+pedroph23@users.noreply.github.com>
2018-05-25 21:40:35 -0300

    Add files via upload

--- exercicio-pol/js/controller.js
+++ exercicio-pol/js/controller.js
@@ -3,6 +3,10 @@ $( document ).ready(function() {
     console.log( "Iniciando.." );
     //$(".progress").hide;
 
+	function trim(str) {
+		return str.replace(/\s{2,}/g, '');
+	}
+
    $("button").click(function() {
     
     //setTimeout(function(){$(".progress").show;}, 1000);
@@ -10,14 +14,15 @@ $( document ).ready(function() {
         $(".progress").hide;
         var simbolos=["+","-","*","/"];
         var txt = $("#entrada").val();
-        console.log(simbolos[3]);
+	var txtNovo = trim(txt);
+        console.log(txt);
 
         
-            if(txt===""){
+            if( txtNovo===""){
                 M.toast({html: 'Campo vazio'});
                 return null;
             }else{
-                txtArray = txt.split("");
+                txtArray =  txtNovo.split("");
 
                 //  if(txt =null){
                 //     M.toast({html: 'Campo vazio'});
@@ -29,18 +34,20 @@ $( document ).ready(function() {
                             console.log(txtArray);
                     
                     
-                            //            9          +                5           â€“            2
+                            //            9          +                5           –            2
                           var inicio = txtArray[0] +  txtArray[1] + txtArray[2]+ txtArray[3] + txtArray[4];
-                            //            9              5           +              2            â€“
-                           var  meio = txtArray[0] +  txtArray[2] + txtArray[1]+ txtArray[4] + txtArray[3];
+ 				//            9          +                5         2		  –            
+                          var meio1= txtArray[0] +  txtArray[1] + txtArray[2]+ txtArray[4] + txtArray[3];
+                            //            9              5           +              2            –
+                           var  meio2 = txtArray[0] +  txtArray[2] + txtArray[1]+ txtArray[4] + txtArray[3];
                             //            9              5           2               +             -            
                             var fim = txtArray[0] +  txtArray[2] + txtArray[4]+ txtArray[1] + txtArray[3];
         
                             
-                            // window.onbeforeunload = function() {
-                                console.log(inicio+"\n"+meio+"\n"+fim);
-                                 $("#resultado").val(inicio+"\n"+meio+"\n"+fim);
-                            //  }
+                            window.onbeforeunload = function() {
+                                console.log(inicio+"\n"+meio1+"\n"+meio2+"\n"+fim);
+                                 return $("#resultado").val(inicio+"\n"+meio1+"\n"+meio2+"\n"+fim);
+                             }
                     
                             }else{
                     
